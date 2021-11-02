660ad66ee6c0c6d2195964c2dc9848e8710c5080
IcePic <icepic2009@googlemail.com>
2021-11-02 15:59:49 +0100

    Add more documentation

--- coroutine_examples/example3.cpp
+++ coroutine_examples/example3.cpp
@@ -1,5 +1,34 @@
 // Add custom awaiteable type
 
+// When a coroutine begins execution, it performs the following:
+//
+// 1) allocates the coroutine state object using operator new (see
+// below)
+//
+// 2) copies all function parameters to the coroutine state: by-value
+// parameters are moved or copied, by-reference parameters remain
+// references (and so may become dangling if the coroutine is resumed
+// after the lifetime of referred object ends)
+
+// 3) calls the constructor for the promise object. If the promise
+// type has a constructor that takes all coroutine parameters, that
+// constructor is called, with post-copy coroutine
+// arguments. Otherwise the default constructor is called.
+
+// 4) calls promise.get_return_object() and keeps the result in a local
+// variable. The result of that call will be returned to the caller
+// when the coroutine first suspends. Any exceptions thrown up to and
+// including this step propagate back to the caller, not placed in the
+// promise.
+
+// 5) calls promise.initial_suspend() and co_awaits its
+// result. Typical Promise types either return a suspend_always, for
+// lazily-started coroutines, or suspend_never, for eagerly-started
+// coroutines.
+
+// 6) when co_await promise.initial_suspend() resumes, starts
+// executing the body of the coroutine
+
 // What does `co_await expr` do?:
 // 1) expr is converted to an awaitable ...
 // 2) the awaiter object is obtained (from the awaitable) ...
